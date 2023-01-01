df889273dd83491467f0dd0c75deab878a270958
makanai <nonoise.zero@gmail.com>
2023-01-02 00:28:36 +0900

    ホーム画面大体　出来上がり

--- app/src/main/java/com/example/androidqr_java/MainActivity.java
+++ app/src/main/java/com/example/androidqr_java/MainActivity.java
@@ -3,12 +3,18 @@ package com.example.androidqr_java;
 import android.accounts.Account;
 import android.accounts.AccountManager;
 import android.content.Intent;
+import android.graphics.Point;
+import android.graphics.drawable.AnimationDrawable;
 import android.hardware.Camera;
 import android.net.Uri;
 import android.os.Bundle;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.Display;
+import android.view.WindowManager;
+import android.view.animation.TranslateAnimation;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.view.LayoutInflater;
@@ -37,6 +43,10 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.widget.Toolbar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.dynamicanimation.animation.DynamicAnimation;
+import androidx.dynamicanimation.animation.FlingAnimation;
+
 import com.example.androidqr_java.databinding.ActivityMainBinding;
 import com.linecorp.linesdk.Constants;
 import com.linecorp.linesdk.LoginDelegate;
@@ -53,7 +63,16 @@ import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
+    //topLay
     private ActivityMainBinding binding;
+    boolean top_layout_frag = false;
+    int translationTop;
+    ImageView buttonBatu;
+    AnimationDrawable batu_animation;
+    FlingAnimation batu_dynamicAnimation;
+
+    //qr
+    boolean qrcode_frag = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,36 +80,63 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        ConstraintLayout constraintLayout = binding.maConstraintLayout;
+        AnimationDrawable animationDrawable = (AnimationDrawable) constraintLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(2000);
+        animationDrawable.setExitFadeDuration(4000);
+        animationDrawable.start();
+
+        //topAnimation
+        //横移動
+        WindowManager wm = (WindowManager)getSystemService(WINDOW_SERVICE);
+        Display disp = wm.getDefaultDisplay();
+        Point realSize = new Point();
+        disp.getRealSize(realSize);
+        DisplayMetrics metrics = getApplicationContext().getResources().getDisplayMetrics();
+
+        translationTop = realSize.x - (int) (60 * metrics.density);
+        binding.maTopLayout.setTranslationX(-translationTop);
+
+        batu_dynamicAnimation = new FlingAnimation(binding.maTopLayout, DynamicAnimation.TRANSLATION_X);
+
         //QR code生成
-        final Button buttonQR = binding.buttonQR;
+        final ImageView buttonQR = binding.maQrcode;
         buttonQR.setOnClickListener(nvoQR);
 
         //cameraへ移動
-        final Button buttonCamera = binding.camera;
+        final ImageView buttonCamera = binding.maScan;
         buttonCamera.setOnClickListener(nvoCamera);
 
         //secondActivityへ遷移
-        final Button buttonSecond = binding.information;
+        final ImageView buttonSecond = binding.maInfo;
         buttonSecond.setOnClickListener(nvoSecond);
 
+        //✕ボタン
+        buttonBatu = binding.maBatuHam;
+        buttonBatu.setOnClickListener(nvoBatu);
     }
 
     //ボタン処理の変数化
     private View.OnClickListener nvoQR = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
-            //文字列の取得
-            String text = binding.editText.getText().toString();
-
-            // QRCodeの作成
-            Bitmap qrCodeBitmap = createQRCode(text);
-
-            // QRCodeの作成に成功した場合
-            if (qrCodeBitmap != null)
-            {
-                // 結果をImageViewに表示
-                binding.imageView.setImageBitmap(qrCodeBitmap);
+            if(qrcode_frag){
+                binding.imageView.setImageResource(R.drawable.aim_trans);
             }
+            else {
+                //文字列の取得
+                //tring text = binding.editText.getText().toString();
+
+                // QRCodeの作成
+                Bitmap qrCodeBitmap = createQRCode("a");
+
+                // QRCodeの作成に成功した場合
+                if (qrCodeBitmap != null) {
+                    // 結果をImageViewに表示
+                    binding.imageView.setImageBitmap(qrCodeBitmap);
+                }
+            }
+            qrcode_frag = !qrcode_frag;
         }
     };
 
@@ -142,7 +188,7 @@ public class MainActivity extends AppCompatActivity {
                 }
                 else
                 {
-                    pixels[offset + x] = Color.WHITE;
+                    pixels[offset + x] = getColor(R.color.clear);
                 }
             }
         }
@@ -166,4 +212,26 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
+    private View.OnClickListener nvoBatu = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            batuAnimation();
+            top_layout_frag = !top_layout_frag;
+        }
+    };
+
+    void batuAnimation(){
+        //✕ ⇔ =
+        if (top_layout_frag){
+            buttonBatu.setBackgroundResource(R.drawable.ma_animation_batu_re);
+            batu_dynamicAnimation.setStartVelocity(-3500).setMinValue(-3000).setMaxValue(5000);
+        }
+        else {
+            buttonBatu.setBackgroundResource(R.drawable.ma_animation_batu);
+            batu_dynamicAnimation.setStartVelocity(3500).setMinValue(-3000).setMaxValue(5000);
+        }
+        batu_animation = (AnimationDrawable) buttonBatu.getBackground();
+        batu_animation.start();
+        batu_dynamicAnimation.start();
+    }
 }
